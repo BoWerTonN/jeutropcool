501aad4002cf154e3f6555576b7fdf8190066caf
daryl coddeville <daryl67160@hotmail.fr>
2022-03-21 19:56:49 +0100

    first commit

---
+++ map/Nat.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Nat" tilewidth="16" tileheight="16" tilecount="480" columns="30">
+ <image source="RPG Nature Tileset2.png" trans="ff678b" width="480" height="256"/>
+</tileset>
---
+++ map/RPG Nature Tileset3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="RPG Nature Tileset3" tilewidth="16" tileheight="16" tilecount="480" columns="30">
+ <image source="RPG Nature Tileset3.png" trans="ffffff" width="480" height="256"/>
+</tileset>
